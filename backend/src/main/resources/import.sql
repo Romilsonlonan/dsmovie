e0f76fbedb54de9c39eab4f33f3a084088135566
Romilson Luis <romilsonlonan@gmail.com>
2022-03-21 09:59:58 -0300

    Domain model, database seed

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -34,9 +34,9 @@ INSERT INTO tb_movies(score, count, title, image) VALUES (0, 0, 'Contato', 'http
 INSERT INTO tb_movies(score, count, title, image) VALUES (0, 0, 'Duna', 'https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jYEW5xZkZk2WTrdbMGAPFuBqbDc.jpg');
 INSERT INTO tb_movies(score, count, title, image) VALUES (0, 0, 'Aquaman', 'https://www.themoviedb.org/t/p/w533_and_h300_bestv2/2cUsDz4TzFYHrKktT1bKHHQ7Cgm.jpg');
 
-INSERT INTO tb_score(movie_id, user_id, value) VALUES (1, 1, 5.0);
-INSERT INTO tb_score(movie_id, user_id, value) VALUES (1, 2, 4.0);
-INSERT INTO tb_score(movie_id, user_id, value) VALUES (2, 1, 3.0);
-INSERT INTO tb_score(movie_id, user_id, value) VALUES (2, 2, 3.0);
-INSERT INTO tb_score(movie_id, user_id, value) VALUES (2, 3, 4.0);
+INSERT INTO tb_score(movies_id, user_id, value) VALUES (1, 1, 5.0);
+INSERT INTO tb_score(movies_id, user_id, value) VALUES (1, 2, 4.0);
+INSERT INTO tb_score(movies_id, user_id, value) VALUES (2, 1, 3.0);
+INSERT INTO tb_score(movies_id, user_id, value) VALUES (2, 2, 3.0);
+INSERT INTO tb_score(movies_id, user_id, value) VALUES (2, 3, 4.0);
 
